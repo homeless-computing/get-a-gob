0d404af05a6af2af24a2d1d3957941796e9c72e5
sucker <lowlifer@protonmail.ch>
2018-03-03 14:09:32 +0300

    have no idea what im doung

---
+++ get-a-job/got-it/polls/models.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
+
+# Create your models here.
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        now = timezone.now()
+    #   return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
